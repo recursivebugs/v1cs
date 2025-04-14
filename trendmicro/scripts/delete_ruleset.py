8fc85d8cb3164454271a6616c8a6460a8b6a527d
Franz Fiorim <franzvitor@gmail.com>
2025-04-14 13:19:55 -0400

    Update delete_ruleset.py

--- trendmicro/scripts/delete_ruleset.py
+++ trendmicro/scripts/delete_ruleset.py
@@ -2,8 +2,8 @@ import requests
 import sys
 import os
 
-api_key = os.getenv("API_KEY")
 ruleset_id = os.getenv("RULESET_ID")
+api_key = os.getenv("API_KEY")
 
 url = f"https://api.xdr.trendmicro.com/beta/containerSecurity/rulesets/{ruleset_id}"
 headers = {
@@ -14,11 +14,11 @@ headers = {
 try:
     res = requests.delete(url, headers=headers)
     if res.status_code == 204:
-        print(f"Ruleset {ruleset_id} deleted.")
+        print(f"Ruleset '{ruleset_id}' deleted successfully.")
         sys.exit(0)
     else:
         print(f"[ERROR] Failed to delete ruleset: {res.status_code} {res.text}")
         sys.exit(1)
 except Exception as e:
-    print(f"[ERROR] Exception during deletion: {e}")
+    print(f"[EXCEPTION] {e}")
     sys.exit(1)

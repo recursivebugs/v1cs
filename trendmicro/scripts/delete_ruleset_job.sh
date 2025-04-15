80c4a18efa78ef54db5466fd8107277da5061fbc
Franz Fiorim <franzvitor@gmail.com>
2025-04-15 09:21:05 -0400

    removing debug scripts

--- trendmicro/scripts/delete_ruleset_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🧹 Cleaning up Ruleset ID '${RULESET_ID}'..."
-
-curl -s -X DELETE "${API_URL}/managedRules/${RULESET_ID}" -H "Authorization: Bearer ${API_KEY}" -H "accept: application/json"
-
-echo "✅ Ruleset deleted successfully."
